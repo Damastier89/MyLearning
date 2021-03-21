64cb2c879a3103afe682be1992bcbc697200706d
sergei <example@mail.r>
2021-03-21 15:43:15 +0300

    add this

---
+++ lessons/lesson_26.THIS.js
@@ -0,0 +1,62 @@
+"use strict";
+// 1) обычная функция: this -> window bat 'use strict' this -> undefined
+function showThis(a, b) {
+    console.log(this);
+
+    function sum() {
+        console.log(this);
+        return a + b;
+    }
+    console.log(sum());
+}
+showThis(4, 5);
+
+// 2) this in objeck -> always this objeck!!!
+//Если вызвать функцию внутри метода, то контекст потеряется.
+const obj = {
+    a: 20,
+    b: 25,
+    sum: function () {
+        function shout() {
+            console.log(this);
+        }
+        shout();
+    }
+};
+
+obj.sum();
+
+// 3) this в конструкторах и классах ссылается на только что созданный новый объект.
+
+function User(name, age) {
+    this.name = name;
+    this.age = age;
+    this.human = true;
+    this.hello = function () {
+        console.log(`Hello ${this.name}`);
+    };
+}
+
+let sergei = new User('Sergei', 31);
+
+function sayName(surname) {
+    console.log(this);
+    console.log(this.name + surname);
+}
+
+const user = {
+    name: 'Sergei',
+};
+
+sayName.call(user, ' Lantsev');
+sayName.apply(user, [' Lantsev']);
+
+function count(num) {
+    return this * num;
+}
+
+const double = count.bind(2); // this это 2
+// теперь double это функция
+console.log(double(3));
+console.log(double(33));
+console.log(double(333));
