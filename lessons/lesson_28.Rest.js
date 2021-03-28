2751d3146fe9c8260b989ab11d4363079ac72632
sergei <example@mail.r>
2021-03-28 17:44:26 +0300

    add rest

--- lessons/lesson_28.Rest.js
+++ lessons/lesson_28.Rest.js
@@ -3,3 +3,9 @@ const log = function (a, b, ...rest) {
 };
 
 log('basic', 'rest', 'a', 'b', 'c');
+
+function calcOrDuble(number, basis = 2) {
+    console.log(number * basis);
+}
+
+calcOrDuble(2, 3);
