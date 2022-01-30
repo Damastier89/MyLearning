92ef6c3794aa5db3b46f277e54b579b7973101f8
sergei <example@mail.r>
2022-01-30 21:06:13 +0300

    Add method object

--- tasks_and_examples/object.js
+++ tasks_and_examples/object.js
@@ -8,3 +8,31 @@ console.log(Object.getOwnPropertyNames(user)); // ['name', 'age', 'isAdmin']
 console.log(Object.keys(first)); // ['0', '1', '2', '3', '4']
 console.log(Object.getOwnPropertyNames(first)); // ['0', '1', '2', '3', '4', 'length']
 
+// Для перебора объекта и редактирования его параметров
+
+var PresentationElement = function (name, operationType, params, baseUesInstance, entityType = null) {
+  this._name = name;
+  this._params = params;
+  this._baseUesInstance = baseUesInstance;
+  this._entityType = this.createEntityType(operationType, entityType);
+  this._type = "element";
+  this._operationType = operationType;
+}
+
+PresentationElement.prototype.editParams = function(presentationProperty, presentationParams = null) {
+  if (presentationProperty) {
+      Object.entries(presentationProperty).forEach(([key, value]) => {
+        if (value !== undefined && value !== null) {
+          this[_key] = value;
+      }
+    })
+  };
+
+  if (presentationParams) {
+      Object.entries(presentationParams).forEach(([key, value]) => {
+        if (value !== undefined && value !== null) {
+          this._params[key] = value;
+      }
+    })
+  };
+}
