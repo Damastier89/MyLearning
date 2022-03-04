11426aaf35ca2611c252cd221c62687127197f75
sergei <example@mail.r>
2022-03-04 19:36:10 +0300

    Update task and  exemples

--- tasks_and_examples/string.js
+++ tasks_and_examples/string.js
@@ -30,3 +30,19 @@ function isStringByOnlySpaces(str) {
   str.replace(/^\s+/i, '').replace(/\s+$/i, '') === '' ? null : res = false;
   return res;
 }  
+
+function kebab(str) {
+  return str.replace(/\b([A-Z][a-z]*)+\b/g, n => n.replace(/([A-Z])/g, '-$1').replace(/^-/, '').toLowerCase());
+};
+kebab('KebabCase noKebabCase No_Kebab_Case And FuckDonaldTrump!!'); // kebab-case noKebabCase No_Kebab_Case and fuck-donald-trump!!
+
+function camelize(str) {
+  let stepOne = str.split("_"); // разбивает str на массив
+  let stepTwo = stepOne.map((word, index) => {  // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
+    return index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+  });
+  let result = stepTwo.join("");
+  return result;
+}
+const result = camelize("list_style_imag") // listStyleImag
+console.log(result)
