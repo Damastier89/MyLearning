bab3602c87278dd687c63b8ba7d9fb53319a6450
sergei <example@mail.r>
2022-08-30 21:45:19 +0300

    Corrected observable data service

--- need_to_know/typeScript/observable data services/index.ts
+++ need_to_know/typeScript/observable data services/index.ts
@@ -1,44 +1,42 @@
-// class HTTPServise {
-//   //Передаём HttpClient ангуляра в конструктор
-//   constructor(private readonly http: HttpClient){}
+export class HTTPServise {
+  //Передаём HttpClient ангуляра в конструктор
+  constructor(private readonly http: HttpClient){}
   
-//   public sendRequest() {
-//   return this.http.get<T>('https://ru.wikipedia.org/wiki/Cp')
-//    }
-//   }
-//   ///////////////////////////////////////////////////////////////////
-//   class DataService {
+  public sendRequest(): Observable<any> {
+    return this.http.get<any>('https://ru.wikipedia.org/wiki/Cp');
+  }
+}
+///////////////////////////////////////////////////////////////////
+export class DataService {
+  //Знак бакса в конце имени, обычно означает, что это какой-то observable
+  private readonly dataList$: Subject<any> = new Subject();
+  //Передаём в наш сервис, cервис отвечающий за запросы в констуктор
+  constructor(private readonly httpServise: HTTPServise) { }
   
-//   //Знак бакса в конце имени, обычно означает, что это какой-то observable
-//   private readonly dataList$: Subject<T> = new Subject();
-//   //Передаём в наш сервис, cервис отвечающий за запросы в констуктор
-//   constructor(private readonly httpServise: HTTPServise) { }
+  public getData(): void {
+  //Когда вызовем этот метод, httpService вернёт какой-то ответ и мы передадим его в subject
+    this.httpServise.sendRequest().subscribe(response => this.dataList$.next(response));
+  }
   
-//   public getData() {
-//   //Когда вызовем этот метод, httpService вернёт какой-то ответ и мы передадим его в subject
-//   this.httpService.sendRequest.subscribe(response => this.dataList.next(response)
-//    }
+  public getDataSubscription(): Observable<any> {
+    return dataList$.asObservable()
+  }
+} 
+////////////////////////////////////////////////////////////////////
+export class MyComponent {
+  public data = null;
   
-//   public getDataSubscription(){
-//   return dataList$.asObservable()
-//    }
-//   } 
-//   ////////////////////////////////////////////////////////////////////
-//   class MyComponent() {
-//   public data = null;
+  constructor(private readonly dataService: DataService) { }
   
-//   constructor(private readonly dataService: DataService) { }
+  ngOnInit() {
+  //Получаем данные с сервера
+  this.dataService.getData();
+  //Передаём данные в компонент
+  this.dataService.getDataSubscription().subscribe(subcriptionData => this.data = subcriptionData);
+  }
   
-//    public ngOnInit() {
-//   //Получаем данные с сервера
-//   this.dataService.getData();
-//   //Передаём данные в компонент
-//   this.dataService.getDataSubscription.subscribe(subcriptionData => this.data = subcriptionData)
-//   }
-  
-//   // Так как у нас подписка, мы теперь можем отправить кучу запросов, и если данные поменяются, то они будут присваиваться нашему полю с данными
-  
-//   public sendResponseByInterval() {
-//   setInterval(() => this.dataService.getData(), 10000)
-//    }
-//   }
+  // Так как у нас подписка, мы теперь можем отправить кучу запросов, и если данные поменяются, то они будут присваиваться нашему полю с данными
+  public sendResponseByInterval() {
+    setInterval(() => this.dataService.getData(), 10000)
+  }
+}
